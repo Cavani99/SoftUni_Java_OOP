1a0fe01abecfc35deaa7069e49132a38f3e51d01
Cavani99 <kristiian.donchev@abv.bg>
2024-11-05 19:45:41 +0200

    Made task 3 in Polymorphism - Lab

---
+++ Polymorphism - Lab/src/Animals/Animal.java
@@ -0,0 +1,34 @@
+package Animals;
+
+public abstract class Animal {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    public void setFavouriteFood(String favouriteFood) {
+        this.favouriteFood = favouriteFood;
+    }
+
+    private String favouriteFood;
+
+    public Animal(String name, String favouriteFood){
+        this.setName(name);
+        this.setFavouriteFood(favouriteFood);
+    }
+
+    public String explainSelf(){
+        return String.format("I am %s and my favourite food is %s",
+                this.getName(),
+                this.getFavouriteFood());
+    };
+}
