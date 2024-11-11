119e5a5642a0f2e78ce0fbf0cc0ba0c40dc93def
Cavani99 <kristiian.donchev@abv.bg>
2024-11-11 20:41:59 +0200

    Made task 5 in Reflection - Lab

---
+++ Reflection - Lab/src/Coding_Tracker/Tracker.java
@@ -0,0 +1,42 @@
+package Coding_Tracker;
+
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Tracker {
+
+    @Author(name = "George")
+    public static void main(String[] args) {
+        Tracker.printMethodsByAuthor(Tracker.class);
+    }
+
+    @Author(name = "Peter")
+    public static void printMethodsByAuthor(Class<?> cl) {
+        Map<String, List<String>> methodsByAuthor = new HashMap<>();
+        Method[] methods = cl.getDeclaredMethods();
+
+        for (Method method : methods) {
+            Author annotation = method.getAnnotation(Author.class);
+
+            if (annotation != null) {
+                methodsByAuthor
+                        .putIfAbsent(annotation.name(), new ArrayList<>());
+                methodsByAuthor
+                        .get(annotation.name()).add(method.getName() + "()");
+            }
+        }
+
+        for (Map.Entry<String, List<String>> entry : methodsByAuthor.entrySet()){
+            String classes = String.join(", ", entry.getValue());
+            System.out.printf("%s: %s\n", entry.getKey(), classes);
+
+        }
+
+
+    }
+
+}
